713fa0c33d0ee09e58af8b8e1fde430967146273
eavictor <eavictor@users.noreply.github.com>
2020-11-25 17:25:25 +0800

    Add windows recovery environment comment.
    Not adding new command, because uuid may be different by system.

--- Windows_EnableWinDbg.bat
+++ Windows_EnableWinDbg.bat
@@ -2,9 +2,20 @@
 
 ECHO Please enter USB controller busparams
 ECHO The value is usually either 0.13.0 or 0.20.0
+ECHO 0.13.0 usually assigned to Thunderbolt.
 SET /p busParam="Enter USB controller busparams: "
 ECHO USB Controller busparams is %busParam%
 ECHO.
+
+REM Enable WinDBG on Windows Recovery Environment
+REM 1. list all sections.
+REM bcdedit /enum all
+REM 2. check description "Windows Recovery Environment"
+REM 3. see identifier uuid. e.g. {915b4b11-53f4-11e8-8b80-84cd3e685088}
+REM 4. Enable debug as below.
+REM bcdedit /set {915b4b11-53f4-11e8-8b80-84cd3e685088} debug on
+REM bcdedit /set {915b4b11-53f4-11e8-8b80-84cd3e685088} testsigning on
+REM bcdedit /set {915b4b11-53f4-11e8-8b80-84cd3e685088} nointegritychecks on
 
 ECHO Enable WinDbg
 bcdedit /bootdebug {bootmgr} on
